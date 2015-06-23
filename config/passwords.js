ec5e91f8cf5d07e9fff9f3ddd5d84801c27b575f
Bailey Blankenship <blankenship15b@ncssm.edu>
2015-06-22 23:32:01 -0400

    removed passwords

--- config/passwords.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  googleClient: '115445966511-ogupss8jo9f6o75egqe5lo4tlvnmmrab.apps.googleusercontent.com',
-  googleSecret: 'xZAr6l55zfyeiMCJ6Szjo2mz',
-  tokenKey: 'ASSFARTS',
-  accountCreationKey: 'booty'
-};
